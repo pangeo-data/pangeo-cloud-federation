d1570cc44d833e4e925681794cca189b83927bbc
Joseph Hamman <jhamman@ucar.edu>
2019-03-28 16:56:40 -0700

    update staging deployment

--- deployments/icesat2/efs_config/setup.sh
+++ deployments/icesat2/efs_config/setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-NAMESPACE=efs
+NAMESPACE=icesat2-staging
+
+kubectl create namespace ${NAMESPACE}
+kubectl --namespace=${NAMESPACE} apply -f ./efs.yaml
+kubectl --namespace=${NAMESPACE} apply -f ./efs_claim.yaml
+
+NAMESPACE=icesat2-prod
 
 kubectl create namespace ${NAMESPACE}
 kubectl --namespace=${NAMESPACE} apply -f ./efs.yaml
