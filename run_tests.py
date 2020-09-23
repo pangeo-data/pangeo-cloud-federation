a1776f6d23df9ec7f4d415914801467a7ab12ab3
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-09-23 15:15:51 -0500

    CI: print output

--- run_tests.py
+++ run_tests.py
@@ -9,4 +9,13 @@ if __name__ == "__main__":
     # use hubploy to handle auth, and call the tests in a subprocess.
     deployment = sys.argv[1]
     with hubploy.auth.cluster_auth(deployment):
-        subprocess.check_output(['./run_tests.sh'])
+        process = subprocess.Popen(["./run_tests.sh"],
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT)
+        while True:
+            output = process.stdout.readline().decode()
+            if output == "" and process.poll() is not None:
+                break
+            if output:
+                print(output.strip())
+        sys.exit(process.poll())
